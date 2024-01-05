488cebbde3a9af20dae5ce53b54ef0a8d4ee241b
Ten-Doka <tdolker51@gmail.com>
2024-01-04 22:30:21 -0500

    Added weather details

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 function currentWeather(response) {
-  //console.log(response.data.city);
+  console.log(response.data);
   let cityElement = document.querySelector("h1");
   cityElement.innerHTML = response.data.city;
+
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  let weatherConditionElement = document.querySelector(".description");
+  weatherConditionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
+
 function searchWeatherElement(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let apiKey = "a15064fo064a4647d4a8bf3bt0bb11df";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(currentWeather);
 }
 
